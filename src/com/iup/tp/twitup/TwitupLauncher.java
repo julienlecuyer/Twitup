35f1b2f14656a7312e7b65570e8883973380751e
Lerouxei <etienne-lr@hotmail.fr>
2017-02-07 16:53:58 +0100

    Launcher JFX et Sw<ing

--- src/com/iup/tp/twitup/TwitupLauncher.java
+++ src/com/iup/tp/twitup/TwitupLauncher.java
@@ -3,29 +3,24 @@ package com.iup.tp.twitup;
 import com.iup.tp.twitup.core.Twitup;
 import com.iup.tp.twitup.mock.MockController;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 /**
  * Classe de lancement de l'application.
  * 
  * @author S.Lucas
  */
-public class TwitupLauncher extends Application {
+public class TwitupLauncher {
 
 	/**
 	 * Launcher.
 	 * 
 	 * @param args
 	 */
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		Twitup twitup = new Twitup();
+		twitup.show();
 		
+		// JAVAFX
+		MockController mockController = new MockController(twitup.getDatabase());
+		mockController.startMock();
 	}
-	
-    public static void main(String[] args) { 
-        launch(args); 
-    } 
 }
--- src/com/iup/tp/twitup/ihm/IJFXView.java
+++ src/com/iup/tp/twitup/ihm/IJFXView.java
@@ -2,6 +2,6 @@ package com.iup.tp.twitup.ihm;
 
 import javafx.scene.layout.Pane;
 
-public interface IJFXView {
+public interface IJFXView extends IView{
 	Pane getPane();
 }
