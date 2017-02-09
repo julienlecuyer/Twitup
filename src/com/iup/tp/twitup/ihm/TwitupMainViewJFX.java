a0e0209fd319e7da6bb90428d55f85b4f865377f
julienlecuyer <lecuyer.ju@gmail.com>
2017-02-09 15:26:17 +0100

    loginview jfx

--- src/com/iup/tp/twitup/ihm/TwitupMainViewJFX.java
+++ src/com/iup/tp/twitup/ihm/TwitupMainViewJFX.java
@@ -2,16 +2,18 @@ package com.iup.tp.twitup.ihm;
 
 import java.awt.Component;
 
-import javax.swing.JFileChooser;
-
-import com.iup.tp.twitup.common.Constants;
-import com.iup.tp.twitup.common.PropertiesManager;
 import com.iup.tp.twitup.core.EntityManager;
 import com.iup.tp.twitup.core.TwitupJFX;
-import com.iup.tp.twitup.core.TwitupS;
 
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class TwitupMainViewJFX implements IMainView<IJFXView> {
@@ -20,11 +22,12 @@ public class TwitupMainViewJFX implements IMainView<IJFXView> {
 	 * Fenetre du bouchon
 	 */
 	
-	 GridPane root;
-	 Scene scene;
+	GridPane root;
 	
 	protected Stage mStage;
-	
+	protected Scene mScene;
+	protected Group mGroup;
+	protected MenuBar mMenuBar;
 	/**
 	 * Base de donénes de l'application.
 	 */
@@ -47,28 +50,67 @@ public class TwitupMainViewJFX implements IMainView<IJFXView> {
 
 	public TwitupMainViewJFX(TwitupJFX ctrl, Stage s) {
 		this.ctrl = ctrl;
+		mStage = s;
 	}
 
 	/**
 	 * Lance l'afficahge de l'IHM.
 	 */
-	public void showGUI(Stage s) {
-		
+	public void showGUI() {
+		initGUI();
 	}
 
 	/**
 	 * Initialisation de l'IHM
 	 */
 	protected void initGUI() {
+		GridPane root = new GridPane();
+		mScene = new Scene(root, 800, 600);
+//		mStage.sizeToScene();
+		mStage.setTitle("Test");
+		mStage.setScene(mScene);
+		mStage.show();
+		initRoot(root);
+	}
 
+	protected void initRoot(GridPane root)
+	{
+		mMenuBar = this.createMenuBar();
+		root.add(mMenuBar, 0, 0);
+		GridPane.setHgrow(mMenuBar, Priority.ALWAYS);
 
-		// Création de la fenetre principale
-		final GridPane root = new GridPane();
+		mGroup = new Group();
+		GridPane.setHgrow(mGroup, Priority.ALWAYS);
+		GridPane.setHalignment(mGroup, HPos.CENTER);
+		GridPane.setVgrow(mGroup, Priority.ALWAYS);
+		GridPane.setValignment(mGroup, VPos.CENTER);
 
-		final Scene scene = new Scene(root, 800, 600);
-		
-
+		root.add(mGroup, 0, 1);
 		ctrl.initLogin();
+//		mGroup.getChildren().add(dummyLoginView);
+	}
+
+	protected void changeContainer() {
+//		mGroup.getChildren().clear();
+//		Node dummySecond = this.createDummySecondComponent();
+//		mGroup.getChildren().add(dummySecond);
+	}
+
+	protected MenuBar createMenuBar() {
+		MenuItem item1 = new MenuItem("i1");
+		MenuItem item2 = new MenuItem("i2");
+		MenuItem item3 = new MenuItem("i3");
+
+		Menu menu1 = new Menu("First");
+		menu1.getItems().addAll(item1, item2);
+
+		Menu menu2 = new Menu("Second");
+		menu2.getItems().addAll(item3);
+
+		MenuBar mb = new MenuBar();
+		mb.getMenus().addAll(menu1, menu2);
+
+		return mb;
 	}
 
 	public void initCreate() {
@@ -80,16 +122,10 @@ public class TwitupMainViewJFX implements IMainView<IJFXView> {
 
 	@Override
 	public void showView(IJFXView v) {
+		mGroup.getChildren().clear();
+		mGroup.getChildren().add(v.getPane());
 	}
 	
 	public void chooseFile(Component parent) {
-		JFileChooser chooser = new JFileChooser();
-		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		int returnVal = chooser.showOpenDialog(parent);
-		if(returnVal == JFileChooser.APPROVE_OPTION) {
-			TwitupS.getProp().setProperty("EXCHANGE_DIRECTORY", chooser.getSelectedFile().getPath());
-			PropertiesManager.writeProperties(TwitupS.getProp(), Constants.CONFIGURATION_FILE);
-			System.out.println(chooser.getSelectedFile().getPath());
-		}
 	}
 }
